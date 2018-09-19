8fbe1e2491b2e5537064ef614cf380da329251ab
Michelle Tang <msmetang@gmail.com>
2018-09-19 11:37:10 -0700

    alignment of icons in mobile view

--- assets/app.js
+++ assets/app.js
@@ -178,6 +178,12 @@ $("#about-btn").click(function() {
     return false;
 });
 
+$("#mobile-about-btn").click(function() {
+    $("#aboutModal").modal("show");
+    $(".navbar-collapse.in").collapse("hide");
+    return false;
+});
+
 $("#nav-btn").click(function() {
     $(".navbar-collapse").collapse("toggle");
     return false;
